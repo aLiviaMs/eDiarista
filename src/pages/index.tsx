6db489a58ffba98bd8372f80b418c83e655deddd
Lívia Minucelli <liviawrk@gmail.com>
2021-10-07 14:53:24 -0300

    Creating SafeEnviroment, UserInformation and Header from aplication

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import SafeEnvironment from 'ui/components/feedback/SafeEnviroment/SafeEnvironment';
 import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
+import UserInformation from 'ui/components/UserInformation/UserInformation';
 
 const Home: NextPage = () => {
   return (
@@ -9,6 +10,18 @@ const Home: NextPage = () => {
       <PageTitle
         title={'Conheça os profissionais'}
         subtitle={'Preencha o endereço e veja todos os profissionais da sua localidade'}
+      />
+      <UserInformation
+        name={'Livia Minucelli'}
+        rating={4}
+        picture={'https://github.com/aLiviaMs.png'}
+        description={'São Paulo'}
+      />
+      <UserInformation
+        name={'Fulano de tal'}
+        rating={3}
+        picture={''}
+        description={'São Paulo'}
       />
     </>
   )
--- src/ui/components/data-display/PageTitle/PageTitle.tsx
+++ src/ui/components/data-display/PageTitle/PageTitle.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { PageTitleContainer, PageTitleStyled } from './PageTitle.style';
+import { PageTitleContainer, PageTitleStyled, PageSubtitleStyled } from './PageTitle.style';
 
 interface PageTitleProps {
     title: string;
@@ -14,7 +14,9 @@ const PageTitle: React.FC<PageTitleProps> = (props) => {
                 <PageTitleStyled>
                     {props.title}
                 </PageTitleStyled>
-                {props.subtitle}
+                <PageSubtitleStyled>
+                    {props.subtitle}
+                </PageSubtitleStyled>
             </PageTitleContainer>
         </>
     )
