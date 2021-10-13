9e5ca17b7499c72f861b08d60e1ed631d552c790
Lívia Minucelli <liviawrk@gmail.com>
2021-10-13 19:28:01 -0300

    finishing design of application and starting some logic parts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,27 @@ import type { NextPage } from 'next';
 import SafeEnvironment from 'ui/components/feedback/SafeEnviroment/SafeEnvironment';
 import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 import UserInformation from 'ui/components/UserInformation/UserInformation';
+import TextFieldMask from 'ui/components/inputs/TextFieldMask/TextFieldMask';
+import { Button, Typography, Container, CircularProgress } from '@mui/material';
+import {
+  FormElementsContainer,
+  ProfissionaisPaper,
+  ProfissionaisContainer
+} from '@styles/index.style';
+import useIndex from 'data/hooks/pages/useIndex.page';
 
 const Home: NextPage = () => {
+  const { cep,
+    setCep,
+    cepValido,
+    buscarProfissionais,
+    erro,
+    diaristas,
+    buscaFeita,
+    carregando,
+    diaristasRestantes
+  } = useIndex();
+
   return (
     <>
       <SafeEnvironment />
@@ -11,18 +30,76 @@ const Home: NextPage = () => {
         title={'Conheça os profissionais'}
         subtitle={'Preencha o endereço e veja todos os profissionais da sua localidade'}
       />
-      <UserInformation
-        name={'Livia Minucelli'}
-        rating={4}
-        picture={'https://github.com/aLiviaMs.png'}
-        description={'São Paulo'}
-      />
-      <UserInformation
-        name={'Fulano de tal'}
-        rating={3}
-        picture={''}
-        description={'São Paulo'}
-      />
+
+      <Container>
+        <FormElementsContainer>
+          <TextFieldMask
+            mask={'99.999-999'}
+            label={'Digite seu CEP'}
+            fullWidth
+            variant={'outlined'}
+            value={cep}
+            onChange={(event) => setCep(event.target.value)}
+          />
+
+          {erro && <Typography color={'error'}>{erro}</Typography>}
+
+          <Button
+            variant={'contained'}
+            color={'secondary'}
+            sx={{ width: '220px' }}
+            disabled={!cepValido || carregando}
+            onClick={() => buscarProfissionais(cep)}
+          >
+            {carregando ? <CircularProgress size={20} /> : 'Buscar'}
+          </Button>
+        </FormElementsContainer>
+
+        {buscaFeita &&
+          (diaristas.length > 0 ? (
+            <ProfissionaisPaper>
+              <ProfissionaisContainer>
+
+                {diaristas.map((item, index) => {
+                  return (
+                    <UserInformation
+                      key={index}
+                      name={item.nome_completo}
+                      picture={item.foto_usuario}
+                      rating={item.reputacao}
+                      description={item.cidade}
+                    />
+                  );
+                })}
+
+              </ProfissionaisContainer>
+
+              <Container sx={{ textAlign: 'center' }} >
+                {diaristasRestantes > 0 && (
+                  <Typography sx={{ mt: 5 }}>
+                    ... e mais {diaristasRestantes} {diaristasRestantes > 1 ? 'profissionais atendem' : 'profissional atende'} ao seu endereço.
+                  </Typography>
+                )}
+
+                <Button
+                  variant={'contained'}
+                  color={'secondary'}
+                  sx={{ mt: 5 }}
+                >
+                  Contratar um profissional
+                </Button>
+              </Container>
+            </ProfissionaisPaper>
+          ) : (
+            <Typography align={'center'} color={'textPrimary'} marginBottom={'2rem'}>
+              Ainda não temos nenhuma diarista disponível em sua região.
+            </Typography>
+          )
+          )}
+
+
+      </Container>
+
     </>
   )
 }
